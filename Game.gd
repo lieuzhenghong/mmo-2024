fd8f6681f7f5e9a0ebd6a8dc47cb700f2d12fc1e
Donald Lee <donaldleeqs@gmail.com>
2024-07-27 23:36:46 +0800

    populate players from gamestate

--- Game.gd
+++ Game.gd
@@ -1,22 +1,27 @@
 extends Node2D
-var player
+
+# Dictionary of int (id) -> Player
+# We will populate this from gamestate.
+var players = {}
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = preload("res://Player.tscn").instantiate()
-	add_child(player)
-	player.position = Vector2(100, 100)  # Starting position
+	var player = preload("res://Player.tscn")
+	for id in Globals.gamestate.keys():
+		player.instantiate()
+		add_child(player)
+		player.position = Globals.gamestate[id]
+		players[id] = player
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var id = multiplayer.get_unique_id()
-	if Globals.gamestate.has(id):
-		player.position = Globals.gamestate[id]
-	else:
-		pass
-		# print("Game._process can't find id %d" % id)
+func _process(delta):	
+	for id in players.keys():
+		var player = players[id]
+		if Globals.gamestate.has(id):
+			player.position = Globals.gamestate[id]
 	
 func _input(event):
 	# Check if the input event is a mouse button press
 	if event is InputEventMouseButton and event.pressed:
 		# Update the target position to the mouse click position
-		player.position = event.position
+		 # player.position = event.position
