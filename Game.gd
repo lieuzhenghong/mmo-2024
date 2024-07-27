9422086a9ebb69c993f8197ccdab0e1cf05fa441
Donald Lee <donaldleeqs@gmail.com>
2024-07-28 00:02:14 +0800

    pass vector2 to update gamestat

--- Game.gd
+++ Game.gd
@@ -34,4 +34,4 @@ func _input(event):
 	if event is InputEventMouseButton and event.pressed:
 		# Update the target position to the mouse click position
 		 # player.position = event.position
-		pass
+		Globals._request_update_gamestate(event.position)
